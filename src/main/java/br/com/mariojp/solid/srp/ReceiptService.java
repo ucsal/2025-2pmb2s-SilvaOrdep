e77eff684a74e393895e79be5203afe0893a3952
PEDRO HENRIQUE OLIVEIRA SILVA <200033264@ucsal.br>
2025-08-21 09:26:35 -0300

    feat: ReceiptFormatter criado para refatoração da formatação de receita

--- src/main/java/br/com/mariojp/solid/srp/ReceiptService.java
+++ src/main/java/br/com/mariojp/solid/srp/ReceiptService.java
@@ -5,19 +5,13 @@ import java.util.List;
 public class ReceiptService {
 	public String generate(Order order) {
 		ITaxCalculator taxCalculator = new TaxCalculator();
+		ReceiptFormatter receiptFormatter = new ReceiptFormatter();
 		double subtotal = getSubtotal(order);
 		double tax = taxCalculator.calcTax(subtotal);
 		double total = subtotal + tax;
 		StringBuilder sb = new StringBuilder(); 
-		sb.append("=== RECIBO ===\n");
-		for (var i : order.getItems()) {
-			sb.append(i.getName()).append(" x").append(i.getQuantity()).append(" = ").append(i.getUnitPrice() * i.getQuantity())
-					.append("\n");
-		}
-		sb.append("Subtotal: ").append(subtotal).append("\n");
-		sb.append("Tax: ").append(tax).append("\n");
-		sb.append("Total: ").append(total).append("\n");
-		return sb.toString();
+
+		return receiptFormatter.formatter(order, tax, subtotal, total);
 	}
 	
 	private double getSubtotal(Order order) {
